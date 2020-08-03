76ba8de5cc5f121b3ef5ef3c40a8f7090e585a6c
RdotSilva <RyandotSilva@gmail.com>
2020-08-03 17:42:56 -0400

    Add matches to read CSV file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("TypeScript entry point");
+import fs from "fs";
+
+// Read the entire contents of the CSV file
+const matches = fs.readFileSync("football.csv", {
+  encoding: "utf-8",
+});
+
+console.log(matches);
