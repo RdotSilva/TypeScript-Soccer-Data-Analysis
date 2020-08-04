a5b6d7bcdf3cfd3a0dca4f79140d7930f2cc5698
RdotSilva <RyandotSilva@gmail.com>
2020-08-04 18:34:44 -0400

    Refactor match analysis using MatchResult object

--- src/index.ts
+++ src/index.ts
@@ -10,19 +10,19 @@ const parsedMatches = matches.split("\n").map((row: string): string[] => {
   return row.split(",");
 });
 
-// Add hard coded values for Home and Away win
-// BAD way of doing it
-const homeWin = "H";
-const awayWin = "A";
-const draw = "D";
+const MatchResult = {
+  HomeWin: "H",
+  AwayWin: "A",
+  Draw: "D",
+};
 
 let manUnitedWins = 0;
 
 // Loop through parsed matches and find the times that Man United wins.
 for (let match of parsedMatches) {
-  if (match[1] === "Man United" && match[5] === homeWin) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === awayWin) {
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
