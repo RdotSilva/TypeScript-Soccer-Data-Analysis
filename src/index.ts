56a2c59e486c7c391f72060480b873a680a78baf
RdotSilva <RyandotSilva@gmail.com>
2020-08-04 18:30:03 -0400

    Add bad example for home/away wins

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,19 @@ const parsedMatches = matches.split("\n").map((row: string): string[] => {
   return row.split(",");
 });
 
+// Add hard coded values for Home and Away win
+// BAD way of doing it
+const homeWin = "H";
+const awayWin = "A";
+const draw = "D";
+
 let manUnitedWins = 0;
 
 // Loop through parsed matches and find the times that Man United wins.
 for (let match of parsedMatches) {
-  if (match[1] === "Man United" && match[5] === "H") {
+  if (match[1] === "Man United" && match[5] === homeWin) {
     manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === "A") {
+  } else if (match[2] === "Man United" && match[5] === awayWin) {
     manUnitedWins++;
   }
 }
