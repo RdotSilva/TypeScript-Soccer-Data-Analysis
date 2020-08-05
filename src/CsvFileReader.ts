ec222c7ae39ba25109d91ddab2544676035edc5f
RdotSilva <RyandotSilva@gmail.com>
2020-08-05 19:04:44 -0400

    Add read method

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,5 +1,18 @@
+import fs from "fs";
+
 export class CsvFileReader {
   data: string[][] = [];
 
   constructor(public filename: string) {}
+
+  read(): void {
+    this.data = fs
+      .readFileSync("football.csv", {
+        encoding: "utf-8",
+      })
+      .split("\n")
+      .map((row: string): string[] => {
+        return row.split(",");
+      });
+  }
 }
