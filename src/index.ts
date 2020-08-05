355b6dc267a18ab245e3b177cde909bff66969c1
RdotSilva <RyandotSilva@gmail.com>
2020-08-05 19:08:02 -0400

    Add reader execution

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import { CsvFileReader } from "./CsvFileReader";
 
 const reader = new CsvFileReader("football.csv");
 
+// Execute the read method and assign the data
+reader.read();
+
 // Match outcome results
 enum MatchResult {
   HomeWin = "H",
@@ -12,7 +15,7 @@ enum MatchResult {
 let manUnitedWins = 0;
 
 // Loop through parsed matches and find the times that Man United wins.
-for (let match of parsedMatches) {
+for (let match of reader.data) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
