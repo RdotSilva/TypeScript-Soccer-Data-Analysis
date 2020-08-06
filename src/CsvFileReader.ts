89aebe5b4c258fe0e93e3ccc8fc295f4139a1bcc
RdotSilva <RyandotSilva@gmail.com>
2020-08-06 19:00:12 -0400

    Update map w/ new values

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -16,7 +16,11 @@ export class CsvFileReader {
         return row.split(",");
       }).map((row: string[]): any  => {
         return [
-          dateStringToDate(row[0])
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4])
         ]
       })
   }
