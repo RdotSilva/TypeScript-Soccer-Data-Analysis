e5883e7e5da0eded903654ad1040dda4fcfabeb3
RdotSilva <RyandotSilva@gmail.com>
2020-08-07 18:18:36 -0400

    Add mapRow method

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -2,8 +2,6 @@ import fs from "fs";}
 import { dateStringToDate } from './utils';
 import { MatchResult } from "./MatchResult";
 
-// TODO: Refactor CsvFileReader to be reusable. As it stands there are too many references to MatchData and this code can only be used for a very specific CSV file.
-
 // Tuple used to describe how the match data should be mapped
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
@@ -30,16 +28,18 @@ export class CsvFileReader {
       .split("\n")
       .map((row: string): string[] => {
         return row.split(",");
-      }).map((row: string[]): MatchData => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult,
-          row[6]
-        ]
-      })
+      }).map(this.mapRow);
+  }
+
+  mapRow(row: string[]): MatchData {
+    return [
+      dateStringToDate(row[0]),
+      row[1],
+      row[2],
+      parseInt(row[3]),
+      parseInt(row[4]),
+      row[5] as MatchResult,
+      row[6]
+    ]
   }
 }
