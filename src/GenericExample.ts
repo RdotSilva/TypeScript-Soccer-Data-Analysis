839c2eba9dbe9aab20b345a7cb10927895e5a35d
RdotSilva <RyandotSilva@gmail.com>
2020-08-08 16:37:55 -0400

    Add HoldString class

--- src/GenericExample.ts
+++ src/GenericExample.ts
@@ -2,5 +2,12 @@ class HoldNumber {
   data: number;
 }
 
+class HoldString {
+  data: string;
+}
+
 const holdNumber = new HoldNumber();
 holdNumber.data = 123;
+
+const holdString = new HoldString();
+holdString.data = "test";
