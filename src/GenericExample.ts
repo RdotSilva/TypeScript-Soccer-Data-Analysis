3919cd61c2f875bf9469e5692508b03ecca3213a
RdotSilva <RyandotSilva@gmail.com>
2020-08-08 16:42:01 -0400

    Add HoldAnything class

--- src/GenericExample.ts
+++ src/GenericExample.ts
@@ -1,3 +1,4 @@
+// Bad example creating new classes for each data type
 class HoldNumber {
   data: number;
 }
@@ -11,3 +12,14 @@ holdNumber.data = 123;
 
 const holdString = new HoldString();
 holdString.data = "test";
+
+// Good example using Generics. Do this instead of the above
+class HoldAnything<TypeOfData> {
+  data: TypeOfData;
+}
+
+const holdAnythingNumber = new HoldAnything<number>();
+holdAnythingNumber.data = 1;
+
+const holdAnythingString = new HoldAnything<string>();
+holdAnythingString.data = "test";
