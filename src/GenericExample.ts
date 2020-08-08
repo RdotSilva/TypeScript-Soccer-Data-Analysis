249bfdfd9145743f94bc2dc81d6958e6698b84a8
RdotSilva <RyandotSilva@gmail.com>
2020-08-08 16:46:17 -0400

    Update HoldAnything class

--- src/GenericExample.ts
+++ src/GenericExample.ts
@@ -14,12 +14,17 @@ const holdString = new HoldString();
 holdString.data = "test";
 
 // Good example using Generics. Do this instead of the above
-class HoldAnything<TypeOfData> {
-  data: TypeOfData;
+class HoldAnything<T> {
+  data: T;
+
+  add(a: T): T {
+    return a;
+  }
 }
 
 const holdAnythingNumber = new HoldAnything<number>();
 holdAnythingNumber.data = 1;
+holdAnythingNumber.add(10);
 
 const holdAnythingString = new HoldAnything<string>();
 holdAnythingString.data = "test";
