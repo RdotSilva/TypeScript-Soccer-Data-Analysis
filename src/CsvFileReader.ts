f87026f27b7ac514780efbf46b86b31d344c4460
RdotSilva <RyandotSilva@gmail.com>
2020-08-09 16:45:43 -0400

    Add read method

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -4,4 +4,15 @@ export class CsvFileReader {
   data: string[][] = [];
 
   constructor(public filename: string) {}
+
+  read(): void {
+    this.data = fs
+      .readFileSync(this.filename, {
+        encoding: "utf-8",
+      })
+      .split("\n")
+      .map((row: string): string[] => {
+        return row.split(",");
+      });
+  }
 }
