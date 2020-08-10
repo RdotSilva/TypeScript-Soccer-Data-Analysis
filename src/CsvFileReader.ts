f6d515c77456cff29b424e21fdf570827c879f7a
RdotSilva <RyandotSilva@gmail.com>
2020-08-10 18:01:42 -0400

    Add JSDoc comments

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,10 +1,19 @@
 import fs from "fs";
 
+/**
+ * A class that is used to read a CSV file
+ * @export
+ * @class CsvFileReader
+ */
 export class CsvFileReader {
   data: string[][] = [];
 
   constructor(public filename: string) {}
 
+  /**
+   * Read the data by splitting at a each new line
+   * @memberof CsvFileReader
+   */
   read(): void {
     this.data = fs
       .readFileSync(this.filename, {
