cd349b000ee8a638fe120a57528a763a66d57080
RdotSilva <RyandotSilva@gmail.com>
2020-08-12 19:46:00 -0400

    Add GoalsScoredAnalysis class

---
+++ src/analyzers/GoalsScoredAnalysis.ts
@@ -0,0 +1,5 @@
+import { Analyzer } from "./../Summary";
+import { MatchData } from "../MatchData";
+import { MatchResult } from "../MatchResult";
+
+export class GoalsScoredAnalysis implements Analyzer {}
