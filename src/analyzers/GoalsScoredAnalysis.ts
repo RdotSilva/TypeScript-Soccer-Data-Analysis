fa8eb8889d22c35a6d1009bf4c974a5c60eb9394
RdotSilva <RyandotSilva@gmail.com>
2020-08-12 19:50:10 -0400

    Add run method

--- src/analyzers/GoalsScoredAnalysis.ts
+++ src/analyzers/GoalsScoredAnalysis.ts
@@ -1,5 +1,20 @@
 import { Analyzer } from "./../Summary";
 import { MatchData } from "../MatchData";
-import { MatchResult } from "../MatchResult";
 
-export class GoalsScoredAnalysis implements Analyzer {}
+export class GoalsScoredAnalysis implements Analyzer {
+  constructor(public team: string) {}
+
+  run(matches: MatchData[]): string {
+    let totalGoalsScored = 0;
+
+    for (let match of matches) {
+      if (match[1] === this.team) {
+        totalGoalsScored += match[3];
+      } else if (match[2] === this.team) {
+        totalGoalsScored += match[4];
+      }
+    }
+
+    return `Team ${this.team} scored a total of ${totalGoalsScored} goals this entire season`;
+  }
+}
