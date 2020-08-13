9e54bbcb248d6495601fd6fb26f822a0821b94b2
RdotSilva <RyandotSilva@gmail.com>
2020-08-13 18:16:15 -0400

    Add buildAndPrintReport method

--- src/Summary.ts
+++ src/Summary.ts
@@ -9,8 +9,10 @@ export interface OutputTarget {
 }
 
 export class Summary {
-  constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {
-    // TODO: Implement the buildAndPrintReport method
-    // TODO: It should take in matches and return nothing (void)
+  constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
+
+  buildAndPrintReport(matches: MatchData[]): void {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
   }
 }
