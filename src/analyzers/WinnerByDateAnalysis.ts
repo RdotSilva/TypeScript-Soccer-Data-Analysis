e6ef4c16b45144af1ec2cd9710248b76457e863e
RdotSilva <RyandotSilva@gmail.com>
2020-08-12 20:09:53 -0400

    Add JSDoc comments

--- src/analyzers/WinnerByDateAnalysis.ts
+++ src/analyzers/WinnerByDateAnalysis.ts
@@ -2,13 +2,25 @@ import { Analyzer } from "./../Summary";
 import { MatchData } from "../MatchData";
 import { MatchResult } from "../MatchResult";
 
+/**
+ * Class that analyzes data to return the winner of a match on a specific date
+ * @export
+ * @class WinnerByDateAnalysis
+ * @implements {Analyzer}
+ */
 export class WinnerByDateAnalysis implements Analyzer {
   constructor(public date: Date) {}
 
+  /**
+   * Analyzes the data to get the winner of a match on specific date
+   * @param {MatchData[]} matches - The match data to analyze
+   * @returns {string}
+   * @memberof WinnerByDateAnalysis
+   */
   run(matches: MatchData[]): string {
-    for (let match of matches) {
-      let matchWinner;
+    let matchWinner;
 
+    for (let match of matches) {
       if (match[0] === this.date) {
         if (match[5] === MatchResult.HomeWin) {
           matchWinner = match[1];
@@ -18,8 +30,7 @@ export class WinnerByDateAnalysis implements Analyzer {
           matchWinner = "DRAW";
         }
       }
-
-      return `The winner of the match on ${this.date} was ${matchWinner}`;
     }
+    return `The winner of the match on ${this.date} was ${matchWinner}`;
   }
 }
