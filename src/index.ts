e1659b8633625276d74a42419f1facc185178094
RdotSilva <RyandotSilva@gmail.com>
2020-08-13 18:20:58 -0400

    Update imports

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,12 @@
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResult";
 import { CsvFileReader } from "./CsvFileReader";
+import { Summary } from "./Summary";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 
 // Create an object that satisfies that 'DataReader' interface
 const csvFileReader = new CsvFileReader("football.csv");
 
-// Create an instance of MatchReader and pass in something statisfying the 'DataReader' interface
+// Create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
 const matchReader = new MatchReader(csvFileReader);
-
 matchReader.load();
-
-let manUnitedWins = 0;
-
-// Loop through parsed matches and find the times that Man United wins.
-for (let match of matchReader.matches) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
