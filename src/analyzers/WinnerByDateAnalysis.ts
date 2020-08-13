f317d3287aeccfc8fbbc7799b4cf44f8f96b26e4
RdotSilva <RyandotSilva@gmail.com>
2020-08-12 20:00:11 -0400

    Add TODO for win by date run method

--- src/analyzers/WinnerByDateAnalysis.ts
+++ src/analyzers/WinnerByDateAnalysis.ts
@@ -4,4 +4,10 @@ import { MatchResult } from "../MatchResult";
 
 export class WinnerByDateAnalysis implements Analyzer {
   constructor(public date: Date) {}
+
+  run(matches: MatchData[]): string {
+    // TODO: Write implementation to search matches for a particular date
+    // TODO: Find out who won the match "H" or "A"
+    // TODO: Get the appropriate match[] based on H or A match[1] for H match[2] for A
+  }
 }
