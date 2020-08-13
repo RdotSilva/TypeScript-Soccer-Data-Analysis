582c6f78f37a665f088e5bbff662d521ea6d54b7
RdotSilva <RyandotSilva@gmail.com>
2020-08-13 18:21:17 -0400

    Add new test case for wins analysis

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,14 @@ const csvFileReader = new CsvFileReader("football.csv");
 // Create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
+
+// The match data
+const matches = matchReader.matches;
+
+// Create a summary for Man United wins
+const summary = new Summary(
+  new WinsAnalysis("Man United"),
+  new ConsoleReport()
+);
+
+summary.buildAndPrintReport(matches);
