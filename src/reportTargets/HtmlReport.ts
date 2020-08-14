2b0dfe9329efc5151cdd80b757690eb4f492f180
RdotSilva <RyandotSilva@gmail.com>
2020-08-14 19:12:05 -0400

    Add print method

--- src/reportTargets/HtmlReport.ts
+++ src/reportTargets/HtmlReport.ts
@@ -1,3 +1,12 @@
 import { OutputTarget } from "../Summary";
 
-export class HtmlReport implements OutputTarget {}
+export class HtmlReport implements OutputTarget {
+  print(report: string): void {
+    const html = `
+      <div>
+        <h1>Analysis Output</h1>
+        <div>${report}</div>
+      </div>
+    `;
+  }
+}
